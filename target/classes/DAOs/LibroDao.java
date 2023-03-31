ee5cd676bce9b28596e485c4cb3d5e04a1dd1e9f
Josué Cortez <cc17081@ues.edu.sv>
2023-03-31 12:10:46 -0600

    Modificando SQL de Libros a vistalibros

--- target/classes/DAOs/LibroDao.java
+++ target/classes/DAOs/LibroDao.java
@@ -21,7 +21,7 @@ public class LibroDao {
             Conexion cone = new Conexion();
             con = cone.abrirConexion();
             String sql = "";
-            sql = "SELECT l.[codi libro] as codigolibro, l.titulolibro , l.existencia , c.[nombrecate ria] as nombrecategoria, l.precio, a.nombreautor FROM db_libros.dbo.tb_libro as l inner join db_libros.dbo.tb_autor as a on a.[codi autor] = l.[codi autor] inner join db_libros.dbo.[tb_cate ria] as c on c.[codi cate ria] = l.[codi cate ria]";
+            sql = "SELECT * FROM vistalibros";
             System.out.println("El SQL estados: " + sql);
             PreparedStatement ps = con.prepareStatement(sql);
             resultSet = ps.executeQuery();
